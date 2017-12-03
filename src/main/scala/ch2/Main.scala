1d36325e3e52eae7bc3344067165a3e46c59274d
Bryan Smith <bryanesmith@gmail.com>
2017-12-03 16:06:58 -0500

    ch2: formatResult

--- src/main/scala/ch2/Main.scala
+++ src/main/scala/ch2/Main.scala
@@ -2,17 +2,21 @@ package ch2
 
 object Main {
 
-  def fib(idx: Int): Option[Int] = {
+  def identity(num: Int): Int = num
+
+  def fib(num: Int): Int = {
     @annotation.tailrec
-    def go(n: Int, current: Int, next: Int): Option[Int] = {
-      if (n < 0) None
-      else if (n == 0) Some(current)
+    def go(n: Int, current: Int, next: Int): Int = {
+      if (n <= 0) current
       else go(n - 1, next, current + next)
     }
-    go(idx, 0, 1)
+    go(num, 0, 1)
   }
 
+  def formatResult(name: String, n: Int, f: Int => Int): String =
+    "The %s of %d is %d.".format(name, n, f(n))
+
   def main(args: Array[String]) : Unit = {
-    println(s"fib: ${(0 to 8).flatMap(fib).mkString(", ")}")
+    println(s"fib: ${(0 to 8).map(fib).mkString(", ")}")
   }
 }
--- src/test/scala/ch2/MainTest.scala
+++ src/test/scala/ch2/MainTest.scala
@@ -6,19 +6,29 @@ import org.scalatest.FlatSpec
 class MainTest extends FlatSpec {
   behavior of "A fib"
 
-  it should "return None for negative indices" in {
-    assert(fib(-1).isEmpty)
-    assert(fib(-42).isEmpty)
+  it should "return 0 for negative indices" in {
+    assert(fib(-1) == 0)
+    assert(fib(-42) == 0)
   }
 
   it should "return the correct values for positive indices" in {
-    assert(fib(0).contains(0))
-    assert(fib(1).contains(1))
-    assert(fib(2).contains(1))
-    assert(fib(3).contains(2))
-    assert(fib(4).contains(3))
-    assert(fib(5).contains(5))
-    assert(fib(6).contains(8))
-    assert(fib(7).contains(13))
+    assert(fib(0) == 0)
+    assert(fib(1) == 1)
+    assert(fib(2) == 1)
+    assert(fib(3) == 2)
+    assert(fib(4) == 3)
+    assert(fib(5) == 5)
+    assert(fib(6) == 8)
+    assert(fib(7) == 13)
+  }
+
+  behavior of "A formatResult"
+
+  it should "return correct string for identity" in {
+    assert(formatResult("identity", 42, identity) == "The identity of 42 is 42.")
+  }
+
+  it should "return correct string for fib" in {
+    assert(formatResult("fib", 7, fib) == "The fib of 7 is 13.")
   }
 }
